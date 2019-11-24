c77387382140558cb39e54793f3e8b4b7d1cbe10
Hugh Hyndman <hugh@redleaf.ca>
2019-11-23 21:22:34 -0500

    Port examples to Scala test

---
+++ src/test/resources/KdbTestSuite.q
@@ -0,0 +1,151 @@
+\l spark.q
+
+test02table:([]
+	i:1000?1000000i; 
+	j:til 1000; 
+	f:1000?10.0;
+	p:2020.01.01D0+100000000*til 1000 
+	)
+
+test03:{
+	([] j:til 1000; p:2020.01.01D0+1D*til 1000; cc:1000#enlist "a string")
+	}
+
+test04:test03
+
+test05:{[opt]
+	tbl:([] j:til 1000; p:2018.09.01D0+1D*til 1000; cc:1000#enlist "a string");
+
+	if[-1=opt`partitionid;
+		:0!meta tbl / Return the schema
+	];
+
+	tbl
+	}
+
+test06:{[opt]
+	.sp.setLogLevel .sp.optGet[opt;`loglevel;`warn];
+	.sp.logDebugOptions[opt];
+
+	tbl:([] j:til 1000; p:2018.09.01D0+1D*til 1000; cc:1000#enlist "a string");
+
+	if[-1=opt`partitionid;
+	:0!meta tbl / Return the schema
+	];
+
+	.sp.pruneAndFilter[opt;tbl]
+	}
+
+
+test07table:([]
+	b:10#01b;
+	g:10?0Ng;
+	x:10?0xFF;
+	h:10?100h;
+	ic:10?1000i;
+	j:10?10000j;
+	e:10?100e;
+	f:10?1000f;
+	c:10#"abcdef";
+	s:10#`$"s",/:string each til 100;
+	p:2018.07.01D00:00:00.123456+1000000000*til 10;
+	m:2000.01m+til 10;
+	d:2018.07.01+til 10;
+	z:2018.07.01T0+til 10;
+	t:00:00:00.000+500*til 10;
+	n:0D0+1000000*til 10;
+	u:10#00:00 23:59;
+	v:10#00:00:00 23:59:59
+	)
+
+test07:{[opt]
+	.sp.setLogLevel .sp.optGet[opt;`loglevel;`warn];
+
+	.sp.logDebug "test07[]";
+	.sp.logDebugOptions[opt];
+
+	partitionid:.sp.optGet[opt;`partitionid;-1];
+
+	//
+	// A partition number of -1 means that Spark just wants the schema
+	//
+	if[partitionid=-1;
+		res:0!meta test07table;
+		.sp.logDebugSchema[res]; 
+		:res
+		];
+
+	rtbl:.sp.pruneAndFilter[opt;test07table];
+	.sp.logDebugTable[rtbl];
+	rtbl
+	}
+
+test08table:([]
+		xxc:10#(1#0x0;0x7f80;"x"$til 10);
+		bbc:10#(1#1b;10101b),"b"$flip 2 vs til 10;
+		hhc:10#(1#1h;100 200h;"h"$til 10);
+		iic:10#(1#1i;100 200i;"i"$reverse til 5);
+		jjc:10#(1#1;100 200;0#0j);
+		eec:10#(1#.1e;100 200e;"e"$.1*til 10);
+		ffc:10#(1#.1f;100 200f;"f"$.1*til 10);
+		ccc:(10?26)#\:"abcdefghijklmnopqrstuvwxyz"; 
+		ppc:10#(1#.z.p;2#.z.p;3#.z.p);
+		ddc:10#(1#.z.d;2#.z.d;3#.z.d)
+		);
+
+test08:{[opt]
+	.sp.setLogLevel .sp.optGet[opt;`loglevel;`warn];
+
+	.sp.logDebug "test08[]";
+	.sp.logDebugOptions[opt];
+
+	partitionid:.sp.optGet[opt;`partitionid;-1];
+
+	//
+	// A partition number of -1 means that Spark just wants the schema
+	//
+	if[partitionid=-1;
+		res:0!meta test08table;
+		.sp.logDebugSchema[res]; 
+		:res
+		];
+
+	test08table
+	}
+
+
+test09table:([]
+		gcolumn:0Ng,1?0Ng;
+		hcolumn:0Nh,1h;
+		icolumn:0Ni,2i;
+		jcolumn:0Nj,3j;
+		ecolumn:0Ne,4.4e;
+		fcolumn:0Nf,5.5f;
+		scolumn:``abc;
+		pcolumn:0Np,.z.p;
+		mcolumn:0Nm,2001.01m;
+		dcolumn:0Nd,2018.01.01;
+		zcolumn:0Nz,2018.01.01T0;
+		tcolumn:0Nt,01:02:03.456;
+		ncolumn:0Nn,0D01:02:03;
+		ucolumn:0Nu,12:23;
+		vcolumn:0Nv,12:23:56
+		)
+
+test09:{[opt]
+	.sp.setLogLevel .sp.optGet[opt;`loglevel;`warn];
+
+	.sp.logDebug "test09";
+	.sp.logDebugOptions[opt];
+
+	if[-1=.sp.optGet[opt;`partitionid;-1]; 
+		nullsupport:.sp.optGetBoolean[opt;`nullsupport;1b];
+		:update n:nullsupport from 0!meta test09table; 
+		]
+
+	test09table
+	}
+
+test10:test09
+
+
--- src/test/scala/KdbTestSuite.scala
+++ src/test/scala/KdbTestSuite.scala
@@ -1,18 +1,28 @@
-import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.scalatest._
 import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.DataFrame
+import scala.collection.mutable.{HashMap}
 
-class KdbTestSuite extends FunSuite with BeforeAndAfterAll {
+class KdbTestSuite extends FunSuite with BeforeAndAfterAllConfigMap {
   var spark: SparkSession = _
 
-  override def beforeAll = {
+  /* Set global options */
+  var gopts = new HashMap[String, String]
+  gopts.put("host", "localhost")
+  gopts.put("port", "5000")
+
+
+  override def beforeAll(cm: ConfigMap) = {
     val conf = new SparkConf()
       .setAppName("kdbspark")
       .setMaster("local")
       .set("spark.default.parallelism", "1")
       .set("spark.jars", "/users/hhyndman/dev/spark-2.4.4/kdbspark.jar")
+
+    /* Place test configuration options in global optons for data source reader */
+    for ((k,v) <- cm) {
+      gopts.put(k, v.toString)
+    }
 
     spark = SparkSession
       .builder()
@@ -20,22 +30,120 @@ class KdbTestSuite extends FunSuite with BeforeAndAfterAll {
       .getOrCreate()
   }
 
-  override def afterAll  = {
-    spark.stop
+  override def afterAll(cm: ConfigMap)  = {
+    spark.close
   }
 
-  test("Test01 - Simple end-to-end test") {
+  test("KDBSPARK-01: Simple end-to-end test") {
     val df = spark.read.format("kdb")
-      .option("host", "localhost")
-      .option("port", 5000)
+      .options(gopts)
       .schema("id long")
       .option("qexpr", "([] id:til 10)")
       .load
-    df.show(3)
+    df.show(3, false)
   }
 
-  test("Test02 - What happened here?") {
-    assert(true);
+  test("KDBSPARK-02: Reading a complete table") {
+    val df = spark.read.format("kdb")
+      .options(gopts)
+      .option("table", "test02table")
+      .load
+    df.show(3, false)
   }
+
+  test("KDBSPARK-03: Simple result with schema provided") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .schema("j long, p timestamp, cc string") // Defaults to nullable
+      .option("function", "test03")
+      .option("pushFilters", false) // Function does not support push-down filters
+      .load
+    df.show(5, false)
+  }
+
+  test("KDBSPARK-04: Simple result with schema provided (StructType)") {
+    import org.apache.spark.sql.types._
+
+    val s = StructType(List(
+      StructField("j", LongType, false),
+      StructField("p", TimestampType, false),
+      StructField("cc", StringType, false)
+    ))
+
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .schema(s) // Better control of nullability
+      .option("function", "test04")
+      .option("pushFilters", false)
+      .load
+
+    df.show(5, false)
+  }
+
+  test("KDBSPARK-05: Schema inquiry") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .option("function", "test05")
+      .load
+
+    df.show(5, false)
+  }
+
+  test("KDBSPARK-06: Column pruning and pushdown filters") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .option("function", "test06")
+      .option("pushFilters", true)
+      .load
+
+    df.filter("j>100 and p<=to_timestamp('2020-01-02')").select("p", "j").show(5, false)
+  }
+
+  test("KDBSPARK-07: Atomic data types") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .option("function", "test07")
+      .load
+
+    df.show(5, false)
+  }
+
+  test("KDBSPARK-08: Array data types") {
+    val df = spark.read
+      .format("kdb")
+      .option("function", "test08")
+      .options(gopts)
+      .load
+
+      df.show(5, false)
+  }
+
+  test("KDBSPARK-09: Null support") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .option("function", "test09")
+      .option("nullsupport", true)
+      .load
+
+    df.show(5, false)
+  }
+
+  test("KDBSPARK-10: Not null support") {
+    val df = spark.read
+      .format("kdb")
+      .options(gopts)
+      .option("function", "test10")
+      .option("nullsupport", false)
+      .load
+
+    df.show(5, false)
+  }
+
 }
 
